7bc7ea45cf23c8f50c25d4efc992ebc1425af21d
akenoq <akenoq@yandex.ru>
2017-09-23 12:54:00 +0300

    стартует с hello.html, к которому подключен app.js с hello.pug результатом рендеринга

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const app = express();
 
 const APPLICATION_PORT = 5000;
 
-app.set('views', './static/views');
-app.set('view engine', 'pug');
+// app.set('views', './static/views');
+// app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
     app.use(express.static(__dirname + "/static"));
-    res.render('index', { title: 'Hey', message: 'Hello there!'});
-    // res.sendfile("static/hello.html");
+    // res.render('index', { title: 'Hey', message: 'Hello there!'});
+    res.sendfile("static/hello.html");
 });
 
 let port = process.env.PORT || APPLICATION_PORT;
